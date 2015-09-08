56702f2658d6aa70e1936b535ace4f38c45b2fdf
encho <ensar@farend.net>
2015-09-08 20:33:48 +0100

    Filesystem tools

--- hlte.mk
+++ hlte.mk
@@ -53,6 +53,16 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Filesystem
+PRODUCT_PACKAGES += \
+    e2fsck \
+    fibmap.f2fs \
+    fsck.f2fs \
+    make_ext4fs \
+    mkfs.f2fs \
+    resize2fs \
+    setup_fs
+
 # GPS
 PRODUCT_PACKAGES += \
     gps.msm8974
